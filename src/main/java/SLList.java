60931e81e9cd2ebd515fe64251bf0008b6028d6e
Fan5 <openconvenience@gmail.com>
2022-10-11 21:24:35 +0800

    Add SLList

---
+++ src/main/java/SLList.java
@@ -0,0 +1,67 @@
+public class SLList {
+    /// IntNode class is static because it never uses SLList's instance methods or variables.
+    private static class IntNode {
+        public int item;
+        public IntNode next;
+
+        public IntNode(int i, IntNode n) {
+            item = i;
+            next = n;
+        }
+    }
+
+    /// The first node (if it exists) is at sentinel.next
+    private IntNode sentinel;
+    private int size;
+
+    /// create empty SLList
+    public SLList() {
+        sentinel = new IntNode(63, null);
+        size = 0;
+    }
+
+    public SLList(int x) {
+        sentinel = new IntNode(63, null);
+        sentinel.next = new IntNode(x, null);
+        size = 1;
+    }
+
+    public void addFirst(int x) {
+        sentinel.next = new IntNode(x, sentinel.next);
+        size++;
+    }
+
+    public void addLast(int x) {
+        IntNode p = sentinel;
+        while (p.next != null) {
+            p = p.next;
+        }
+        p.next = new IntNode(x, null);
+        size++;
+    }
+
+//    private static int size(IntNode p) {
+//        if (p.next == null) {
+//            return 1;
+//        }
+//        return 1 + size(p.next);
+//    }
+
+    public int size() {
+//        return size(first);
+        return size;
+    }
+
+    public int getFirst() {
+        return sentinel.next.item;
+    }
+
+    public static void main(String[] args) {
+        SLList L = new SLList(15);
+        L.addFirst(10);
+        L.addFirst(5);
+        L.addLast(20);
+        System.out.println(L.getFirst());
+        System.out.println(L.size());
+    }
+}
