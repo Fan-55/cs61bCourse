ca30782c00e7e00abb1ccb3f1131d96533a8c2f4
Fan5 <openconvenience@gmail.com>
2022-10-25 00:09:23 +0800

    Remove unused code

--- src/main/java/IntList.java
+++ src/main/java/IntList.java
@@ -30,12 +30,4 @@ public class IntList {
         }
         return rest.get(i - 1);
     }
-
-    public static void main(String[] args) {
-        IntList L = new IntList(15, null);
-        L = new IntList(10, L);
-        L = new IntList(5, L);
-        System.out.println(L.size());
-        System.out.println(L.iterativeSize());
-    }
 }
