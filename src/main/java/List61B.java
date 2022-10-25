70a02492387b3db798fbf629c73ab07526393582
Fan5 <openconvenience@gmail.com>
2022-10-25 20:34:57 +0800

    Add addFirst method to List61B

--- src/main/java/List61B.java
+++ src/main/java/List61B.java
@@ -1,7 +1,13 @@
 public interface List61B<Item> {
     public void addLast(Item x);
+
+    public void addFirst(Item x);
+
     public Item getLast();
+
     public int size();
+
     public Item get(int i);
+
     public Item removeLast();
 }
