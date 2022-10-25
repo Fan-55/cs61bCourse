ddf0b53d0748ff14eb0142a5a2fc3eb2ad66a646
Fan5 <openconvenience@gmail.com>
2022-10-25 21:10:12 +0800

    Add implementation interface print method

--- src/main/java/List61B.java
+++ src/main/java/List61B.java
@@ -10,4 +10,10 @@ public interface List61B<Item> {
     public Item get(int i);
 
     public Item removeLast();
+
+    default public void print() {
+        for (int i = 0; i < size(); i += 1) {
+            System.out.print(get(i) + " ");
+        }
+    }
 }
