a0b6be5812f222d0d6fabbf6f8f74e3dd0121f74
Fan5 <openconvenience@gmail.com>
2022-10-25 23:21:38 +0800

    Add HoF

---
+++ src/main/java/IntUnaryFunction.java
@@ -0,0 +1,4 @@
+// Represents a function that takes in an integer, and returns an integer
+public interface IntUnaryFunction {
+    int apply(int x);
+}
---
+++ src/main/java/TenX.java
@@ -0,0 +1,7 @@
+public class TenX implements IntUnaryFunction {
+    // Return 10 times the argument.
+    @Override
+    public int apply(int x) {
+        return 10 * x;
+    }
+}
