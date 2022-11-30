1711ee9e43373c57612f210371b78377f69cf3b0
Fan5 <openconvenience@gmail.com>
2022-11-30 23:31:34 +0800

    Update AList

--- src/main/java/AList.java
+++ src/main/java/AList.java
@@ -1,14 +1,19 @@
 /**
- * Invariants:
- * addLast: The item which is going to be added to the list will be at position size. E.g., The size is 4, and the new item
- * will be at 4th position.
- * size: The number of items in the list should be size.
- * getLast: The last item is at position (size - 1)
+ * items: [1, 2, 3, 50, 9, ...]
+ * size: 5
+ * invariants:
+ * addLast: The added item should be at position size.
+ * size: The size is the number of items in the list.
+ * getLast: The last item is at position size - 1.
  */
-public class AList<Item> implements List61B<Item> {
+
+public class AList<Item> {
     private Item[] items;
     private int size;
 
+    /**
+     * Creates an empty list.
+     */
     public AList() {
         items = (Item[]) new Object[100];
         size = 0;
@@ -20,52 +25,46 @@ public class AList<Item> implements List61B<Item> {
         items = a;
     }
 
-    @Override
+    /**
+     * Inserts X into the back of the list.
+     */
     public void addLast(Item x) {
         if (size == items.length) {
-            resize(size + 1);
+            resize(size * 2);
         }
         items[size] = x;
-        size += 1;
+        size++;
     }
 
-    @Override
+    /**
+     * Returns the item from the back of the list.
+     */
     public Item getLast() {
         return items[size - 1];
     }
 
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
+    /**
+     * Gets the ith item in the list (0 is the front).
+     */
     public Item get(int i) {
         return items[i];
     }
 
-    /* Deletes the time from the back of the list and returns deleted item */
+    /**
+     * Returns the number of items in the list.
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Deletes item from back of the list and
+     * returns deleted item.
+     */
     public Item removeLast() {
-        Item x = getLast();
+        Item lastItem = getLast();
         items[size - 1] = null;
-        size -= 1;
-        return x;
-    }
-
-    @Override
-    public void addFirst(Item x) {
-        insert(x, 0);
-    }
-
-    public void insert(Item x, int position) {
-        Item[] newItems = (Item[]) new Object[items.length + 1];
-
-        System.arraycopy(items, 0, newItems, 0, position);
-        newItems[position] = x;
-
-        System.arraycopy(items, position, newItems, position + 1, items.length - position);
-        items = newItems;
-
-        size += 1;
+        size--;
+        return lastItem;
     }
 }
