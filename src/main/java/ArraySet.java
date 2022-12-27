8d9c57c5f2340412a862317644cc0b5d31294abf
Fan5 <openconvenience@gmail.com>
2022-12-27 22:27:06 +0800

    Implement iterator method

--- src/main/java/ArraySet.java
+++ src/main/java/ArraySet.java
@@ -1,4 +1,6 @@
-public class ArraySet<T> {
+import java.util.Iterator;
+
+public class ArraySet<T> implements Iterable<T> {
     private T[] items;
     private int size;
 
@@ -38,14 +40,58 @@ public class ArraySet<T> {
         return size;
     }
 
+    @Override
+    public Iterator<T> iterator() {
+        return new ArraySetIterator();
+    }
+
+    private class ArraySetIterator implements Iterator<T> {
+        private int wizPos;
+
+        public ArraySetIterator() {
+            this.wizPos = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return wizPos < size;
+        }
+
+        @Override
+        public T next() {
+            T returnItem = items[wizPos];
+            wizPos++;
+            return returnItem;
+        }
+    }
+
     public static void main(String[] args) {
-        ArraySet<String> s = new ArraySet<>();
-        s.add(null);
-        s.add("horse");
-        s.add("fish");
-        s.add("house");
-        s.add("fish");
-        System.out.println(s.contains("horse"));
-        System.out.println(s.size());
+//        ArraySet<String> s = new ArraySet<>();
+//        s.add("horse");
+//        s.add("fish");
+//        s.add("house");
+//        s.add("fish");
+//        System.out.println(s.contains("horse"));
+//        System.out.println(s.size());
+
+        ArraySet<Integer> aset = new ArraySet<>();
+        aset.add(5);
+        aset.add(23);
+        aset.add(42);
+
+        /* under the hood of enhanced for loop */
+        Iterator<Integer> aseer = aset.iterator();
+        while (aseer.hasNext()) {
+            int i = aseer.next();
+            System.out.println(i);
+        }
+
+        /**
+         * To use enhanced for loop, the ArraySet has to implement Iterable interface so that java knows
+         * the class's instance has iterator method
+         */
+        for (int i : aset) {
+            System.out.println(i);
+        }
     }
 }
